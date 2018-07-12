ae8814c6d541f92274838f933c277fa27f217cf4
Elijah Jensen <ejensen141@gmail.com>
2018-07-12 00:09:39 -0400

    Update DataWJS.js

--- DataWJS.js
+++ DataWJS.js
@@ -7,4 +7,14 @@ function get_current_ip_address() // returns the ip address of the current devic
 }
 
 
-var ip_address = 'ws://' + get_current_ip_address() + ":4567/";
+
+
+//sets up a connection to a particular ip and port. 
+function setupConnection(ip,port)
+{	
+	var ip_address = 'ws://' + ip + ":"+port+"/";
+	var ws = new WebSocket(ip_address);
+	ws.binaryType="arraybuffer";
+	
+
+}
