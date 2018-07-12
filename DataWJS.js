9a462868d30fb90dbe81ccb47a80c3b90fd3be10
Elijah Jensen <ejensen141@gmail.com>
2018-07-12 00:04:15 -0400

    Update DataWJS.js

--- DataWJS.js
+++ DataWJS.js
@@ -1,2 +1,10 @@
+//DataWJS Main Code Repository 
+
+function get_current_ip_address() // returns the ip address of the current device
+{
+	console.log(window.location.hostname);
+	return window.location.hostname;
+}
+
 
 var ip_address = 'ws://' + get_current_ip_address() + ":4567/";
